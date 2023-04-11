d0fba6f047a74e8716ada30aa83051c40a336e9c
Simon Redman <simon@ergotech.com>
2023-04-10 21:10:27 -0400

    Change to controller interface of suta_ble_bed

--- suta_mqtt_bridge/suta_mqtt_bridge.py
+++ suta_mqtt_bridge/suta_mqtt_bridge.py
@@ -5,11 +5,13 @@ from mqtt_suta_bed import MqttSutaBed
 from mqtt_bridge import MqttBridge
 from consts import MqttPayload, SUTA_MANUFACTURER
 
-import suta_ble_bed.suta_ble_scanner as suta_scanner
+from suta_ble_bed.suta_ble_bed_controller import SutaBleBedController
 
 import asyncio
 from bleak import BleakError
 import logging
+
+logger = logging.getLogger(__name__)
 
 class SutaMqttBridge:
 
@@ -36,50 +38,24 @@ class SutaMqttBridge:
     async def start(self) -> None:
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.mqtt_bridge.start())
-            tg.create_task(self.start_device_polling())
+            tg.create_task(self.start_device_scanning())
 
-    async def start_device_polling(self) -> None:
-        while True:
-            discovered_devices = [device for device in await suta_scanner.discover(mac = None, adapter = self.adapter, wait = self.bluetooth_wait_interval_seconds)]
-            discovered_device_addresses = [device.address for device in discovered_devices]
-            newly_discovered_devices = [device for device in discovered_devices if device.address not in self.mqtt_bridge.tracked_devices and device not in self.mqtt_bridge.unpaired_devices]
-            for device in newly_discovered_devices:
-                wrapped_bed = MqttSutaBed(BleSutaBed(device))
-                if device.address in self.mqtt_bridge.known_devices:
+    async def start_device_scanning(self) -> None:
+        async with SutaBleBedController() as controller:
+            async for bed in controller.devices():
+                logger.info(f"Discovered {bed.device}")
+                wrapped_bed = MqttSutaBed(bed)
+
+                if bed.device.address in self.mqtt_bridge.known_devices:
                     # This is a device with which we are paired, either due to the pairing having been broken
                     # or due to another device on the same MQTT network having paired.
                     # Presumably, the user wants us to connect to this device as well.
                     # (To prevent this from hapening, delete the device in Home Assistant or manually remove the MQTT topic.)
-                    await wrapped_bed.bed._ensure_connection()
-                    await self.mqtt_bridge.add_tracked_device(key = device.address, device = wrapped_bed)
-                elif not device.address in self.mqtt_bridge.unpaired_devices:
+                    await controller.connect(wrapped_bed.bed)
+                    await self.mqtt_bridge.add_tracked_device(key = bed.device.address, device = wrapped_bed)
+                elif not bed.device.address in self.mqtt_bridge.unpaired_devices:
                     # This is a device we have not seen before. Add it as unpaired.
-                    await self.mqtt_bridge.add_unpaired_device(key = device.address, device = wrapped_bed)
+                    await self.mqtt_bridge.add_unpaired_device(key = bed.device.address, device = wrapped_bed)
 
-            missing_devices = [] # Paired devices which we could not find
-            for addr in self.mqtt_bridge.tracked_devices:
-                device = self.mqtt_bridge.tracked_devices[addr]
-                if addr not in discovered_device_addresses:
-                    # We are supposed to have been controlling this device but it is no longer in range. Mark it as such.
-                    missing_devices.append(addr)
-                else:
-                    try:
-                        await self.mqtt_bridge.enqueue_update(device, online=True)
-                    except BleakError as be:
-                        if addr in self.tracked_mugs:
-                            missing_devices.append(addr)
-                        logging.warning(f"Error while communicating with device: {be}")
-
-            for addr in missing_devices:
-                await self.mqtt_bridge.remove_tracked_device(addr)
-
-            gone_unpaired_device_addresses = set()
-            for unpaired_address in self.mqtt_bridge.unpaired_devices:
-                # Clean up any unpaired devices we no longer see
-                if not unpaired_address in discovered_device_addresses:
-                    gone_unpaired_device_addresses.add(unpaired_address)
-
-            for gone_device_address in gone_unpaired_device_addresses:
-                await self.mqtt_bridge.remove_unpaired_device(gone_device_address)
-
-            await asyncio.sleep(self.update_interval)
+                # Yield the thread to avoid starving anyone else, since the BLE discovery can be noisy
+                await asyncio.sleep(0)
